1b609c4ac6c1f8a31dfc3bb85b00d8b80f1e09dd
manashdewangan29 <manash.dewangan@gloitel.in>
2025-11-17 12:36:32 +0530

    done some responsive changes

--- app/page.jsx
+++ app/page.jsx
@@ -189,22 +189,10 @@ const HomePage = () => {
                   width={1920}
                   height={1080}
                   src="https://res.cloudinary.com/dsqu6pi0d/image/upload/v1762927804/Gloitel/banner_mfm3ss.webp"
-                  alt="Video Thumbnail"
+                  alt="image"
                   className="absolute top-0 left-0 w-full h-full object-cover rounded-2xl"
                   unoptimized
                 />
-                <div className="absolute inset-0 flex items-center justify-center">
-                  <div className="w-16 h-16 sm:w-20 sm:h-20 flex items-center justify-center bg-blue-600/90 rounded-full shadow-lg">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="white"
-                      viewBox="0 0 24 24"
-                      className="w-8 h-8 sm:w-10 sm:h-10"
-                    >
-                      <path d="M8 5v14l11-7z" />
-                    </svg>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
